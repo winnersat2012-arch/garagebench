edd6600b817844c62f648bf4fd381199c6c75581
Wistakio <Wistakio@gmail.com>
2026-09-07 12:22:30 +0200

    build: enable Windows installer packaging

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+    },
+  },
 };
